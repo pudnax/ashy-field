8fce20685de577481b8c4b78ac8fca227b77170b
Alex Komissarov <k.a.komissar@gmail.com>
2020-11-10 00:01:13 +0500

    Add swapchain

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,10 @@ use ash::{
 };
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let event_loop = winit::event_loop::EventLoop::new();
+    let window = winit::window::Window::new(&event_loop)?;
+    let surface_extensions = ash_window::enumerate_required_extensions(&window)?;
+
     let entry = Entry::new()?;
 
     let api_version = match entry.try_enumerate_instance_version()? {
@@ -28,20 +32,25 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .iter()
         .map(|layer_name| layer_name.as_ptr())
         .collect();
-    let extension_name_pointers: Vec<*const i8> =
-        vec![ash::extensions::ext::DebugUtils::name().as_ptr()];
+    let extension_name_pointers: Vec<*const i8> = surface_extensions
+        .into_iter()
+        .chain(vec![ash::extensions::ext::DebugUtils::name()])
+        .map(|ext_name| ext_name.as_ptr())
+        .collect();
 
-    let mut debugcreateinfo = vk::DebugUtilsMessengerCreateInfoEXT {
-        message_severity: vk::DebugUtilsMessageSeverityFlagsEXT::WARNING
+    let mut debugcreateinfo = vk::DebugUtilsMessengerCreateInfoEXT::builder()
+        .message_severity(
+            vk::DebugUtilsMessageSeverityFlagsEXT::WARNING
             // | vk::DebugUtilsMessageSeverityFlagsEXT::VERBOSE
             // | vk::DebugUtilsMessageSeverityFlagsEXT::INFO
             | vk::DebugUtilsMessageSeverityFlagsEXT::ERROR,
-        message_type: vk::DebugUtilsMessageTypeFlagsEXT::GENERAL
-            | vk::DebugUtilsMessageTypeFlagsEXT::PERFORMANCE
-            | vk::DebugUtilsMessageTypeFlagsEXT::VALIDATION,
-        pfn_user_callback: Some(vulkan_debug_utils_callback),
-        ..Default::default()
-    };
+        )
+        .message_type(
+            vk::DebugUtilsMessageTypeFlagsEXT::GENERAL
+                | vk::DebugUtilsMessageTypeFlagsEXT::PERFORMANCE
+                | vk::DebugUtilsMessageTypeFlagsEXT::VALIDATION,
+        )
+        .pfn_user_callback(Some(vulkan_debug_utils_callback));
 
     let instance_create_info = vk::InstanceCreateInfo::builder()
         // For the case of the structs of raw pointers approach
@@ -52,6 +61,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .enabled_extension_names(&extension_name_pointers);
 
     let instance = unsafe { entry.create_instance(&instance_create_info, None)? };
+
+    let surface = unsafe { ash_window::create_surface(&entry, &instance, &window, None)? };
+    let surface_loader = ash::extensions::khr::Surface::new(&entry, &instance);
 
     let debug_utils = ash::extensions::ext::DebugUtils::new(&entry, &instance);
 
@@ -72,11 +84,20 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let queuefamilyproperties =
         unsafe { instance.get_physical_device_queue_family_properties(physical_device) };
-    let qfamindices = {
+    let qfamindlices = {
         let mut found_graphics_q_index = None;
         let mut found_transfer_q_index = None;
         for (index, qfam) in queuefamilyproperties.iter().enumerate() {
-            if qfam.queue_count > 0 && qfam.queue_flags.contains(vk::QueueFlags::GRAPHICS) {
+            if qfam.queue_count > 0
+                && qfam.queue_flags.contains(vk::QueueFlags::GRAPHICS)
+                && unsafe {
+                    surface_loader.get_physical_device_surface_support(
+                        physical_device,
+                        index as u32,
+                        surface,
+                    )?
+                }
+            {
                 found_graphics_q_index = Some(index as u32);
             }
             if qfam.queue_count > 0
@@ -95,24 +116,79 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let priorities = [1.0f32];
     let queue_infos = [
         vk::DeviceQueueCreateInfo::builder()
-            .queue_family_index(qfamindices.0)
+            .queue_family_index(qfamindlices.0)
             .queue_priorities(&priorities)
             .build(),
         vk::DeviceQueueCreateInfo::builder()
-            .queue_family_index(qfamindices.1)
+            .queue_family_index(qfamindlices.1)
             .queue_priorities(&priorities)
             .build(),
     ];
+    let device_extension_name_pointers: Vec<*const i8> =
+        vec![ash::extensions::khr::Swapchain::name().as_ptr()];
     let device_create_info = vk::DeviceCreateInfo::builder()
         .queue_create_infos(&queue_infos)
+        .enabled_extension_names(&device_extension_name_pointers)
         .enabled_layer_names(&layer_name_pointers);
     let logical_device =
         unsafe { instance.create_device(physical_device, &device_create_info, None)? };
-    let graphics_queue = unsafe { logical_device.get_device_queue(qfamindices.0, 0) };
-    let transfer_queue = unsafe { logical_device.get_device_queue(qfamindices.1, 0) };
+    let graphics_queue = unsafe { logical_device.get_device_queue(qfamindlices.0, 0) };
+    let transfer_queue = unsafe { logical_device.get_device_queue(qfamindlices.1, 0) };
+
+    let surface_capabilities = unsafe {
+        surface_loader.get_physical_device_surface_capabilities(physical_device, surface)?
+    };
+    let surface_present_modes = unsafe {
+        surface_loader.get_physical_device_surface_present_modes(physical_device, surface)?
+    };
+    let surface_formats =
+        unsafe { surface_loader.get_physical_device_surface_formats(physical_device, surface)? };
+
+    let queue_families = [qfamindlices.0];
+    let swapchain_create_info = vk::SwapchainCreateInfoKHR::builder()
+        .surface(surface)
+        .min_image_count(
+            3.max(surface_capabilities.min_image_count)
+                .min(surface_capabilities.max_image_count),
+        )
+        .image_format(surface_formats.first().unwrap().format)
+        .image_color_space(surface_formats.first().unwrap().color_space)
+        .image_extent(surface_capabilities.current_extent)
+        .image_array_layers(1)
+        .image_usage(vk::ImageUsageFlags::COLOR_ATTACHMENT)
+        .image_sharing_mode(vk::SharingMode::EXCLUSIVE)
+        .queue_family_indices(&queue_families)
+        .pre_transform(surface_capabilities.current_transform)
+        .composite_alpha(vk::CompositeAlphaFlagsKHR::OPAQUE)
+        .present_mode(vk::PresentModeKHR::FIFO);
+    let swapchain_loader = ash::extensions::khr::Swapchain::new(&instance, &logical_device);
+    let swapchain = unsafe { swapchain_loader.create_swapchain(&swapchain_create_info, None)? };
+
+    let swapchain_images = unsafe { swapchain_loader.get_swapchain_images(swapchain)? };
+    let mut swapchain_imageviews = Vec::with_capacity(swapchain_images.len());
+    for image in &swapchain_images {
+        let subresource_range = vk::ImageSubresourceRange::builder()
+            .aspect_mask(vk::ImageAspectFlags::COLOR)
+            .base_mip_level(0)
+            .level_count(1)
+            .base_array_layer(0)
+            .layer_count(1);
+        let imageview_create_info = vk::ImageViewCreateInfo::builder()
+            .image(*image)
+            .view_type(vk::ImageViewType::TYPE_2D)
+            .format(vk::Format::B8G8R8A8_UNORM)
+            .subresource_range(*subresource_range);
+        let imageview = unsafe { logical_device.create_image_view(&imageview_create_info, None) }?;
+        swapchain_imageviews.push(imageview);
+    }
 
     unsafe {
+        for iv in &swapchain_imageviews {
+            logical_device.destroy_image_view(*iv, None);
+        }
+        swapchain_loader.destroy_swapchain(swapchain, None);
         logical_device.destroy_device(None);
+        surface_loader.destroy_surface(surface, None);
         debug_utils.destroy_debug_utils_messenger(utils_messanger, None);
         instance.destroy_instance(None);
     };
